1c99dd05e8fb07e57b1dabdcb8543b96d2904867
balaprojects <bala.emailme@gmail.com>
2020-09-05 22:54:10 +0530

    HTTP-Basic - InMemory Authentication

---
+++ InMemory-Basic/guest-app/src/main/java/com/frankmoley/security/app/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.frankmoley.security.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.withDefaultPasswordEncoder().username("bala").password("bala").roles("USER", "ADMIN").build());
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+}
